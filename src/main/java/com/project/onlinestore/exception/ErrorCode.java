e87857cbe7ba286b23af4f67e1301146fb69bd9e
김재령 <wofud0321@naver.com>
2024-02-05 00:12:19 +0900

    로그인시 password가 틀릴경우 에러코드 추가

--- src/main/java/com/project/onlinestore/exception/ErrorCode.java
+++ src/main/java/com/project/onlinestore/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "userName is duplicated"),
-    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "userName not found");
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "userName not found"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid");
 
     private HttpStatus status;
     private String errorMessage;
