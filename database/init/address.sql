2313e7c1e1d7db7d5f5d3e3becdaf88186e6c541
김재령 <wofud0321@naver.com>
2024-03-07 08:56:32 +0900

    배송지 관련 Address 엔티티 생성

--- database/init/address.sql
+++ database/init/address.sql
@@ -2,5 +2,10 @@ drop table if exists address;
 
 create table address
 (
-    address_id bigint not null auto_increment primary key
+    address_id bigint not null auto_increment primary key,
+    user_id bigint not null ,
+    postal_code INTEGER not null ,
+    tel varchar(30),
+    address varchar(200) not null ,
+    detail_address varchar(200) not null
 ) engine = InnoDB;
--- src/main/java/com/project/onlinestore/user/entity/Address.java
+++ src/main/java/com/project/onlinestore/user/entity/Address.java
@@ -14,4 +14,20 @@ public class Address {  // 배송지 정보
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ADDRESS_ID")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String address;
+
+    @Column(name = "detail_address")
+    private String detailAddress;
+
+    @Column(name = "postal_code")
+    private Integer postalCode;
+
+    private String tel;
+    // TODO: 배송지 id가 들어와있으면 들어와 있는 애로 배송 하기, 없으면 에러 발생, 무조건 배송지를 만들어 줘서 id 값을 받아와야 한다.
+    // TODO: 스프링 address 엔티티, USER 엔티티에서 List<Address> 가능할까??
 }
