5b831820813203ac742c8d70c5ee1d57c4c65322
김재령 <wofud0321@naver.com>
2024-03-06 18:14:08 +0900

    sql문 데이터 타입 수정

--- database/init/orderItem.sql
+++ database/init/orderItem.sql
@@ -1,11 +1,11 @@
-drop table if exists orderItems;
+drop table if exists order_items;
 
-create table orderItems
+create table order_items
 (
     order_item_id    bigint not null auto_increment primary key,
     order_id bigint not null,
     item_id bigint not null,
-    order_item_count bigint not null,
-    order_price bigint not null
+    order_item_count INTEGER not null,
+    order_price INTEGER not null
 ) engine = InnoDB;
 
