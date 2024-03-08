409248d4ce94819628fc313f606f9f300bd967fd
김재령 <wofud0321@naver.com>
2024-03-09 00:03:09 +0900

    Address 엔티티에 수취인 이름 추가

--- database/init/address.sql
+++ database/init/address.sql
@@ -4,6 +4,7 @@ create table address
 (
     address_id bigint not null auto_increment primary key,
     user_id bigint not null ,
+    addressee_name varchar(20) not null ,
     postal_code INTEGER not null ,
     tel varchar(30),
     address varchar(200) not null ,
--- src/main/java/com/project/onlinestore/user/entity/Address.java
+++ src/main/java/com/project/onlinestore/user/entity/Address.java
@@ -19,6 +19,9 @@ public class Address {  // 배송지 정보
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "addressee_name")
+    private String addresseeName;   // 수취인 이름
+
     private String address;
 
     @Column(name = "detail_address")
@@ -28,6 +31,4 @@ public class Address {  // 배송지 정보
     private Integer postalCode;
 
     private String tel;
-    // TODO: 배송지 id가 들어와있으면 들어와 있는 애로 배송 하기, 없으면 에러 발생, 무조건 배송지를 만들어 줘서 id 값을 받아와야 한다.
-    // TODO: 스프링 address 엔티티, USER 엔티티에서 List<Address> 가능할까??
 }
