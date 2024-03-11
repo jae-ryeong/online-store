247b8dff8c234aa162c31c5a010d1b2815cea0c4
김재령 <wofud0321@naver.com>
2024-03-11 10:45:18 +0900

    controller mapping name을 알맞게 수정

--- src/main/java/com/project/onlinestore/order/controller/OrderController.java
+++ src/main/java/com/project/onlinestore/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.project.onlinestore.order.controller;
 
 import com.project.onlinestore.order.Entity.Order;
 import com.project.onlinestore.order.Entity.OrderItem;
+import com.project.onlinestore.order.dto.request.OrderAddressRequestDto;
 import com.project.onlinestore.order.dto.response.OrderResponseDto;
 import com.project.onlinestore.order.dto.response.OrderViewResponseDto;
 import com.project.onlinestore.order.service.OrderService;
@@ -9,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,16 +21,16 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/list")
-    public ResponseEntity<OrderResponseDto> orderList(Authentication authentication) {
-        OrderResponseDto orderResponseDto = orderService.itemOrder(authentication.getName());
+    @PostMapping("/checkout")
+    public ResponseEntity<OrderResponseDto> orderCheckOut(Authentication authentication, @RequestBody OrderAddressRequestDto dto) {
+        OrderResponseDto orderResponseDto = orderService.itemOrder(authentication.getName(), dto);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(orderResponseDto);
     }
 
-    @GetMapping("/view")
-    public ResponseEntity<List<OrderViewResponseDto>> orderView(Authentication authentication) {
+    @GetMapping("/list")
+    public ResponseEntity<List<OrderViewResponseDto>> orderList(Authentication authentication) {
         List<OrderViewResponseDto> dtoList = orderService.orderView(authentication.getName());
 
         return ResponseEntity.status(HttpStatus.OK)
