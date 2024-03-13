f9cf41d8e453bd982230da29afccf960b541d36a
김재령 <wofud0321@naver.com>
2024-03-13 21:57:12 +0900

    OrderRepository에서 userId로 order를 찾을때 List로 여러 데이터를 찾도록 변경

--- src/main/java/com/project/onlinestore/order/repository/OrderRepository.java
+++ src/main/java/com/project/onlinestore/order/repository/OrderRepository.java
@@ -3,8 +3,8 @@ package com.project.onlinestore.order.repository;
 import com.project.onlinestore.order.Entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findByUser_Id(Long userId);
+    List<Order> findAllByUser_Id(Long userId);
 }
