31df7c58bb0e052e8764400267bac3d9607af33d
김재령 <wofud0321@naver.com>
2024-03-15 13:47:08 +0900

    주문 취소 불가 ErrorCode추가

--- src/main/java/com/project/onlinestore/exception/ErrorCode.java
+++ src/main/java/com/project/onlinestore/exception/ErrorCode.java
@@ -21,7 +21,8 @@ public enum ErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "Order not found"),
     QUANTITY_OUT_OF_RANGE(HttpStatus.CONFLICT, "Cannot be less than 0"),
     DIFFERENT_FROM_THE_PATTERN(HttpStatus.CONFLICT, "It's different from the pattern"),
-    ADDRESS_NOT_FOUNT(HttpStatus.NOT_FOUND, "Address not found");
+    ADDRESS_NOT_FOUNT(HttpStatus.NOT_FOUND, "Address not found"),
+    CAN_NOT_CANCELED(HttpStatus.NOT_MODIFIED, "Orders that have been shipped cannot be canceled.");
 
     private HttpStatus status;
     private String errorMessage;
