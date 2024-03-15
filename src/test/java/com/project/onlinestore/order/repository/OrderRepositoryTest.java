e6ac93ed95db1a8a294f345839539aa2bf3385a1
김재령 <wofud0321@naver.com>
2024-03-15 13:47:53 +0900

    주문 취소시 orderStatus를 cancel로 바꾸는 쿼리 추가 및 테스트

---
+++ src/test/java/com/project/onlinestore/order/repository/OrderRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.project.onlinestore.order.repository;
+
+import com.project.onlinestore.order.Entity.Order;
+import com.project.onlinestore.order.Entity.enums.OrderStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    void updateOrderStatusCancel() {
+        //given
+        Order order = Order.builder().orderStatus(OrderStatus.ORDER).build();
+        orderRepository.save(order);
+
+        //when
+        orderRepository.updateOrderStatusCancel(OrderStatus.CANCEL);
+
+        //then
+        Optional<Order> result = orderRepository.findById(order.getId());
+        assertThat(result.get().getOrderStatus()).isEqualTo(OrderStatus.CANCEL);
+    }
+}
