ab812f34a345a94d81f10c9526f928996bce0818
김재령 <wofud0321@naver.com>
2024-09-10 06:33:42 +0900

    모두

---
+++ frontend/src/components/content.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const ContentWrapper = styled.div`
+    
+`;
+
+export default function Content(){
+    return(
+        <ContentWrapper>
+            
+        </ContentWrapper>
+    )
+}
