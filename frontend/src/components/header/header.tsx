be1a16caf6be656c5d4c38e8d0f4403209b5ac8e
김재령 <wofud0321@naver.com>
2024-09-15 21:35:53 +0900

    header 기본 ui 생성

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,23 +1,34 @@
 import styled from "styled-components"
-import { ImAppleinc } from "react-icons/im";
+import { ImAppleinc} from "react-icons/im";
+import { FaUser } from "react-icons/fa";
+import SearchBar from "./searchBar";
 
 const HeaderContainer = styled.div`
     display: flex; 
     align-items: center;
     justify-content: space-around;
-    width: 100%;
+    width: 1190px;
     height: 100px;
     background-color: white;
 
     .logo{
         font-size: 40px;
     }
-`
+
+    .user {
+      font-size: 40px;
+    }
+`;
 
 export default function Header(){
     return(
       <HeaderContainer>
         <ImAppleinc className="logo"/>
+
+          <SearchBar>
+          </SearchBar>
+
+        <FaUser className="user"/>
       </HeaderContainer>  
     );
 }
---
+++ frontend/src/components/header/searchBar.tsx
@@ -0,0 +1,84 @@
+import styled from "styled-components"
+import { MdOutlineCancel } from "react-icons/md";
+import { PiMagnifyingGlass } from "react-icons/pi";
+import { useState } from "react";
+
+const SearchContainer = styled.form`
+  width: 35%;
+  height: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border-bottom: 1px solid gray;
+
+  .deleteButton{
+    font-size: 20px;
+  }
+  .searchButton{
+    font-size: 25px;
+  }
+`;
+
+const SearchDeleteButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  
+  &:hover{
+    color: #db0f0fd3;
+  }
+`;
+
+const SearchClick = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+const SearchBarContainer = styled.input`
+    width: 80%;
+    height: 80%;
+    font-size: 1.2rem;
+    border:none;
+    outline: none;
+    
+`;
+
+function searchFocus() {
+    
+}
+export default function SearchBar(props:any) { //any 타입 고치기
+    const [searchKeyword, setSearchKeyword] = useState<string>("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchKeyword(e.target.value);
+    }
+
+    const clearSearch = () => {
+      setSearchKeyword(""); // 검색어 초기화
+    };
+
+    return(
+        <SearchContainer>
+            <SearchBarContainer placeholder="검색어를 입력해주세요." 
+            type="text"
+            onFocus={searchFocus}
+            value={searchKeyword}
+            onChange={handleInputChange}
+            > 
+
+            </SearchBarContainer>
+
+            <SearchDeleteButton onClick={clearSearch} type="button">
+              <MdOutlineCancel className="deleteButton"/>
+            </SearchDeleteButton>
+
+            <SearchClick>
+            <PiMagnifyingGlass className="searchButton" />
+            </SearchClick>
+        </SearchContainer>
+        
+    );
+}
