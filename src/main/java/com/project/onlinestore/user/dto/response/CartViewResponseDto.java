e3cd7c77b94ab6035329937a7a99a1a618415a90
김재령 <wofud0321@naver.com>
2025-01-26 22:31:41 +0900

    장바구니관련 responseDTO 수정

--- src/main/java/com/project/onlinestore/user/dto/response/CartViewResponseDto.java
+++ src/main/java/com/project/onlinestore/user/dto/response/CartViewResponseDto.java
@@ -3,12 +3,14 @@ package com.project.onlinestore.user.dto.response;
 import com.project.onlinestore.user.entity.ItemCart;
 
 public record CartViewResponseDto(
+        Long itemCartId,
         String itemName,
         Integer price,
         String storeName,
+        Integer quantity,
         boolean cartCheck
 ) {
     public static CartViewResponseDto fromEntity(ItemCart itemCart) {
-        return new CartViewResponseDto(itemCart.getItem().getItemName(), itemCart.getItem().getPrice(), itemCart.getItem().getUser().getStoreName(), itemCart.isCartCheck());
+        return new CartViewResponseDto(itemCart.getId(), itemCart.getItem().getItemName(), itemCart.getItem().getPrice(), itemCart.getItem().getUser().getStoreName(), itemCart.getQuantity(), itemCart.isCartCheck());
     }
 }
