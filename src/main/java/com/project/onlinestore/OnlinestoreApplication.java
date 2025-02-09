de00e69c77a0b990bcd9c06c5f20ae3b5a312219
김재령 <wofud0321@naver.com>
2025-02-09 10:00:07 +0900

    CORS 전역 설정

--- src/main/java/com/project/onlinestore/OnlinestoreApplication.java
+++ src/main/java/com/project/onlinestore/OnlinestoreApplication.java
@@ -2,7 +2,10 @@ package com.project.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +15,14 @@ public class OnlinestoreApplication {
 		SpringApplication.run(OnlinestoreApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
