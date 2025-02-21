512110432812cd441892424a7e7bc3260ef9a765
김재령 <wofud0321@naver.com>
2025-02-21 21:23:43 +0900

    페이지 네이션 구현을 위한 수정

--- frontend/src/components/MainComponent.tsx
+++ frontend/src/components/MainComponent.tsx
@@ -2,23 +2,27 @@ import styled from "styled-components";
 import Header from "./header/TotalHeader";
 import Banner from "./banner/TotalBanner";
 import Category from "./CategoryMenu/TotalCategory";
-import Contents from "./content/contents";
+import { Outlet } from "react-router-dom";
 
 const MainTotalWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 200vh;
+    min-height: 200vh;
     width: 100vw;
     background-color: #FBFBFB;
 `
 
-export default function MainComponent(){
+interface MainComponentProps {
+    setPage: React.Dispatch<React.SetStateAction<number>>
+}
+
+export default function MainComponent({setPage}:MainComponentProps){
     return(
     <MainTotalWrapper>
         <Header/>
         <Banner/>
-        <Category/>
-        <Contents/>
+        <Category setPage={setPage}/>
+        <Outlet/>
     </MainTotalWrapper>
 );}
