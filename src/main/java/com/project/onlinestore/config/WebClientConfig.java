98a9e9468ecdc7ae73c6efadf8c92889ae9e7def
김재령 <wofud0321@naver.com>
2025-03-12 13:28:11 +0900

    WebClient 빈 추가

---
+++ src/main/java/com/project/onlinestore/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.project.onlinestore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.portone.io")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
