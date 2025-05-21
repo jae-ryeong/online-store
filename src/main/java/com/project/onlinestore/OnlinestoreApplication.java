cbf2ae1adb03e2d5990deba9b616a85424ee6b6d
김재령 <wofud0321@naver.com>
2025-05-22 07:40:26 +0900

    aws 배포 api 수정

--- src/main/java/com/project/onlinestore/OnlinestoreApplication.java
+++ src/main/java/com/project/onlinestore/OnlinestoreApplication.java
@@ -24,7 +24,7 @@ public class OnlinestoreApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(apiUrl + ":3000")
+				registry.addMapping("/**").allowedOrigins(apiUrl)
 						.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
